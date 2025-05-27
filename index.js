b52dc130e675bceb15d673762b2f93155c915e34
Kishukumar07 <kishukumars082@gmail.com>
2025-05-27 07:30:04 +0530

    defined express app

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log('helo this is script .js')
-console.log('helo this is script .js')
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Your middlewares here
+
+app.get('/', (req, res) => {
+  res.end('talking with server');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is live at: http://localhost:${port}`);
+});
